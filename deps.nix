a2440df1162390b18ed9d27f1992b53cc12458b4
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-24 17:56:44 +0800

    mix 2 nix
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

---
+++ deps.nix
@@ -0,0 +1,272 @@
+{ lib, beamPackages, overrides ? (x: y: {}) }:
+
+let
+  buildRebar3 = lib.makeOverridable beamPackages.buildRebar3;
+  buildMix = lib.makeOverridable beamPackages.buildMix;
+  buildErlangMk = lib.makeOverridable beamPackages.buildErlangMk;
+
+  self = packages // (overrides self packages);
+
+  packages = with beamPackages; with self; {
+    certifi = buildRebar3 rec {
+      name = "certifi";
+      version = "2.12.0";
+
+      src = fetchHex {
+        pkg = "certifi";
+        version = "${version}";
+        sha256 = "ee68d85df22e554040cdb4be100f33873ac6051387baf6a8f6ce82272340ff1c";
+      };
+
+      beamDeps = [];
+    };
+
+    crontab = buildMix rec {
+      name = "crontab";
+      version = "1.1.13";
+
+      src = fetchHex {
+        pkg = "crontab";
+        version = "${version}";
+        sha256 = "d67441bec989640e3afb94e123f45a2bc42d76e02988c9613885dc3d01cf7085";
+      };
+
+      beamDeps = [ ecto ];
+    };
+
+    db_connection = buildMix rec {
+      name = "db_connection";
+      version = "2.7.0";
+
+      src = fetchHex {
+        pkg = "db_connection";
+        version = "${version}";
+        sha256 = "dcf08f31b2701f857dfc787fbad78223d61a32204f217f15e881dd93e4bdd3ff";
+      };
+
+      beamDeps = [ telemetry ];
+    };
+
+    decimal = buildMix rec {
+      name = "decimal";
+      version = "2.1.1";
+
+      src = fetchHex {
+        pkg = "decimal";
+        version = "${version}";
+        sha256 = "53cfe5f497ed0e7771ae1a475575603d77425099ba5faef9394932b35020ffcc";
+      };
+
+      beamDeps = [];
+    };
+
+    ecto = buildMix rec {
+      name = "ecto";
+      version = "3.11.2";
+
+      src = fetchHex {
+        pkg = "ecto";
+        version = "${version}";
+        sha256 = "3c38bca2c6f8d8023f2145326cc8a80100c3ffe4dcbd9842ff867f7fc6156c65";
+      };
+
+      beamDeps = [ decimal jason telemetry ];
+    };
+
+    ecto_sql = buildMix rec {
+      name = "ecto_sql";
+      version = "3.11.3";
+
+      src = fetchHex {
+        pkg = "ecto_sql";
+        version = "${version}";
+        sha256 = "e5f36e3d736b99c7fee3e631333b8394ade4bafe9d96d35669fca2d81c2be928";
+      };
+
+      beamDeps = [ db_connection ecto postgrex telemetry ];
+    };
+
+    gen_stage = buildMix rec {
+      name = "gen_stage";
+      version = "1.2.1";
+
+      src = fetchHex {
+        pkg = "gen_stage";
+        version = "${version}";
+        sha256 = "83e8be657fa05b992ffa6ac1e3af6d57aa50aace8f691fcf696ff02f8335b001";
+      };
+
+      beamDeps = [];
+    };
+
+    hackney = buildRebar3 rec {
+      name = "hackney";
+      version = "1.20.1";
+
+      src = fetchHex {
+        pkg = "hackney";
+        version = "${version}";
+        sha256 = "fe9094e5f1a2a2c0a7d10918fee36bfec0ec2a979994cff8cfe8058cd9af38e3";
+      };
+
+      beamDeps = [ certifi idna metrics mimerl parse_trans ssl_verify_fun unicode_util_compat ];
+    };
+
+    httpoison = buildMix rec {
+      name = "httpoison";
+      version = "1.8.2";
+
+      src = fetchHex {
+        pkg = "httpoison";
+        version = "${version}";
+        sha256 = "2bb350d26972e30c96e2ca74a1aaf8293d61d0742ff17f01e0279fef11599921";
+      };
+
+      beamDeps = [ hackney ];
+    };
+
+    idna = buildRebar3 rec {
+      name = "idna";
+      version = "6.1.1";
+
+      src = fetchHex {
+        pkg = "idna";
+        version = "${version}";
+        sha256 = "92376eb7894412ed19ac475e4a86f7b413c1b9fbb5bd16dccd57934157944cea";
+      };
+
+      beamDeps = [ unicode_util_compat ];
+    };
+
+    jason = buildMix rec {
+      name = "jason";
+      version = "1.4.4";
+
+      src = fetchHex {
+        pkg = "jason";
+        version = "${version}";
+        sha256 = "c5eb0cab91f094599f94d55bc63409236a8ec69a21a67814529e8d5f6cc90b3b";
+      };
+
+      beamDeps = [ decimal ];
+    };
+
+    metrics = buildRebar3 rec {
+      name = "metrics";
+      version = "1.0.1";
+
+      src = fetchHex {
+        pkg = "metrics";
+        version = "${version}";
+        sha256 = "69b09adddc4f74a40716ae54d140f93beb0fb8978d8636eaded0c31b6f099f16";
+      };
+
+      beamDeps = [];
+    };
+
+    mimerl = buildRebar3 rec {
+      name = "mimerl";
+      version = "1.3.0";
+
+      src = fetchHex {
+        pkg = "mimerl";
+        version = "${version}";
+        sha256 = "a1e15a50d1887217de95f0b9b0793e32853f7c258a5cd227650889b38839fe9d";
+      };
+
+      beamDeps = [];
+    };
+
+    parse_trans = buildRebar3 rec {
+      name = "parse_trans";
+      version = "3.4.1";
+
+      src = fetchHex {
+        pkg = "parse_trans";
+        version = "${version}";
+        sha256 = "620a406ce75dada827b82e453c19cf06776be266f5a67cff34e1ef2cbb60e49a";
+      };
+
+      beamDeps = [];
+    };
+
+    postgrex = buildMix rec {
+      name = "postgrex";
+      version = "0.18.0";
+
+      src = fetchHex {
+        pkg = "postgrex";
+        version = "${version}";
+        sha256 = "a042989ba1bc1cca7383ebb9e461398e3f89f868c92ce6671feb7ef132a252d1";
+      };
+
+      beamDeps = [ db_connection decimal jason ];
+    };
+
+    quantum = buildMix rec {
+      name = "quantum";
+      version = "3.5.3";
+
+      src = fetchHex {
+        pkg = "quantum";
+        version = "${version}";
+        sha256 = "500fd3fa77dcd723ed9f766d4a175b684919ff7b6b8cfd9d7d0564d58eba8734";
+      };
+
+      beamDeps = [ crontab gen_stage telemetry telemetry_registry ];
+    };
+
+    ssl_verify_fun = buildRebar3 rec {
+      name = "ssl_verify_fun";
+      version = "1.1.7";
+
+      src = fetchHex {
+        pkg = "ssl_verify_fun";
+        version = "${version}";
+        sha256 = "fe4c190e8f37401d30167c8c405eda19469f34577987c76dde613e838bbc67f8";
+      };
+
+      beamDeps = [];
+    };
+
+    telemetry = buildRebar3 rec {
+      name = "telemetry";
+      version = "1.2.1";
+
+      src = fetchHex {
+        pkg = "telemetry";
+        version = "${version}";
+        sha256 = "dad9ce9d8effc621708f99eac538ef1cbe05d6a874dd741de2e689c47feafed5";
+      };
+
+      beamDeps = [];
+    };
+
+    telemetry_registry = buildMix rec {
+      name = "telemetry_registry";
+      version = "0.3.2";
+
+      src = fetchHex {
+        pkg = "telemetry_registry";
+        version = "${version}";
+        sha256 = "e7ed191eb1d115a3034af8e1e35e4e63d5348851d556646d46ca3d1b4e16bab9";
+      };
+
+      beamDeps = [ telemetry ];
+    };
+
+    unicode_util_compat = buildRebar3 rec {
+      name = "unicode_util_compat";
+      version = "0.7.0";
+
+      src = fetchHex {
+        pkg = "unicode_util_compat";
+        version = "${version}";
+        sha256 = "25eee6d67df61960cf6a794239566599b09e17e668d3700247bc498638152521";
+      };
+
+      beamDeps = [];
+    };
+  };
+in self
+
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+	inputs = {
+	  nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+	  flake-utils.url = "github:numtide/flake-utils";	
+	};
+
+	outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = import nixpkgs { inherit system; };
+        webmentions_db = pkgs.beamPackages.mixRelease {
+          pname = "webmentions_db";
+          version = "0.1.0";
+          src = ./.;
+          removeCookie = false;
+          mixNixDeps = with pkgs; import ./deps.nix { 
+            inherit lib beamPackages;
+          };
+        };
+			in with pkgs; rec {
+        packages = { webmentions_db = webmentions_db; };
+        packages.default = packages.webmentions_db;
+				devShells.default = mkShell {
+					buildInputs = with pkgs; [
+						# tools
+            elixir
+            mix2nix
+					];
+				};
+			});
+}
